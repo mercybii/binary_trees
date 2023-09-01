aec16793f26063fc936be4f812370961d650db6b
mercybii <mercybii95@gmail.com>
2023-09-01 21:17:09 +0300

    bs

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,52 +1,91 @@
 #include "binary_trees.h"
 
+bst_t *helper_loop(bst_t *root);
+
 /**
- * bst_remove - Removes a node from a Binary Search Tree (BST).
- * @root: Pointer to the root node of the tree where the
- * node will be removed.
- * @value: Value to remove from the tree.
- *
- * Return: Pointer to the new root node of the tree after
- * removing the value.
+ * bst_remove - removes a node from a BST
+ * @root: pointer to the root node
+ * @value: value to remove
+ * Description: 28. BST - Remove
+ * Return: see below
+ * 1. upon success, return pointer to new root node
+ * 2. upon fail, return NULL
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *temp;
+	/* declare and initialize pointer */
+	bst_t *new = NULL;
 
+	/* if there's no root node or no given value */
 	if (root == NULL)
-		return (NULL);
+		return (root);
 
-	if (value < root->n)
+	/* first search for value on the left subtree */
+	/* then search for value on the right subtree */
+	if (root->n > value)
 		root->left = bst_remove(root->left, value);
-	else if (value > root->n)
+	else if (root->n < value)
 		root->right = bst_remove(root->right, value);
-	else
+	else if (root->n == value)
 	{
-		/* Node with one child or no child */
+		/* if there's no children */
+		if ((root->left == NULL) && (root->right == NULL))
+		{
+			free(root);
+			return (NULL);
+		}
+		/* if there's one right child */
 		if (root->left == NULL)
 		{
-			temp = root->right;
+			new = root->right;
+			if (root->parent)
+			{
+				if (root->parent->left == root)
+					root->parent->left = new;
+				else
+					root->parent->right = new;
+			}
+			new->parent = root->parent;
 			free(root);
-			return (temp);
+			return (new);
 		}
+		/* if there's one left child */
 		else if (root->right == NULL)
 		{
-			temp = root->left;
+			new = root->left;
+			if (root->parent)
+			{
+				if (root->parent->left == root)
+					root->parent->left = new;
+				else
+					root->parent->right = new;
+			}
+			new->parent = root->parent;
 			free(root);
-			return (temp);
+			return (new);
 		}
-
-		/* Node with two children, find the in-order successor */
-		temp = root->right;
-		while (temp->left != NULL)
-			temp = temp->left;
-
-		/* Copy the data of the successor to the current node */
-		root->n = temp->n;
-
-		/* Delete the in-order successor */
-		root->right = bst_remove(root->right, temp->n);
+		/* if there's two children */
+		else
+		{
+			new = helper_loop(root->right);
+			root->n = new->n;
+			root->right = bst_remove(root->right, new->n);
+		}
 	}
-
 	return (root);
+}
+
+/**
+ * helper_loop - searches for in-order successor
+ * @root: pointer to root node
+ * Description: searches in left subtree only
+ * Return: pointer to in-order successor
+ */
+bst_t *helper_loop(bst_t *root)
+{
+	bst_t *current = root;
+
+	while ((current) && (current->left))
+		current = current->left;
+	return (current);
 }
